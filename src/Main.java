456c7ca20953da1c8a4bda415266c80bb788e06b
Godmankind <gimadwww@gmail.com>
2021-03-14 12:21:32 +0400

    add miles

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    int price=258000;
+    double coefficient=0.05;
+    double miles=price*coefficient;
+    System.out.println(miles);
+
+    }
+
+}
